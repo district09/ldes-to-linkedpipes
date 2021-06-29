b2e68d49b921aa982a0d701e4bfac7522e4a2134
Niels Vandekeybus <progster@gmail.com>
2021-06-29 11:40:46 +0200

    export output folder from config

--- client/src/config.js
+++ client/src/config.js
@@ -2,3 +2,4 @@ export const LDES_ENDPOINT = process.env.LDES_ENDPOINT || "https://apidg.gent.be
 export const LINKED_PIPES_PIPELINE= process.env.LINKED_PIPES_PIPELINE || "http://srvlodlppoc01.gentgrp.gent.be:8080/resources/pipelines/1621340292743";
 export const LINKED_PIPES_ENDPOINT = process.env.LINKED_PIPES_ENDPOINT || "http://localhost:9080/";
 export const DIRECT_PUSH = process.env.DIRECT_PUSH === "true";
+export const OUTPUT_FOLDER = process.env.OUTPUT_FOLDER || './';
